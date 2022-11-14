2a71b1d45fcdf21770b7d1be3ad13acff9e67196
Simón Negrín <simon.negrin@gmail.com>
2022-11-14 19:44:21 +0000

    Inproved Joystick component

---
+++ src/lib/components/Chevron.svelte
@@ -0,0 +1,38 @@
+<script>
+
+  export let dir = 'up'
+
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 48 48"
+  class="chevron {dir}"
+  >
+  <path d="m18.75 36-2.15-2.15 9.9-9.9-9.9-9.9 2.15-2.15L30.8 23.95Z"/>
+</svg>
+
+<style>
+
+  .chevron {
+    width: 1em;
+    height: 1em;
+  }
+
+  :global(.chevron.up) {
+    rotate: -90deg;
+  }
+
+  :global(.chevron.left) {
+    rotate: 180deg;
+  }
+
+  :global(.chevron.bottom) {
+    rotate: 90deg;
+  }
+
+  path {
+    fill: currentColor;
+  }
+
+</style>
--- src/lib/components/Joystick.svelte
+++ src/lib/components/Joystick.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createEventDispatcher } from "svelte"
   import { calcDistance, getVectorDir } from "../services/geometry"
+  import Chevron from './Chevron.svelte'
 
   const threshold = 30
   const dispatch = createEventDispatcher()
@@ -42,6 +43,18 @@
   on:touchmove={onTouchstartOrTouchmove}
   on:touchend={onTouchend}
   >
+  <div class="frame">
+    <div class="up">
+      <Chevron dir="up" />
+    </div>
+    <div class="middle">
+      <Chevron dir="left" />
+      <Chevron dir="right" />
+    </div>
+    <div class="bottom">
+      <Chevron dir="bottom" />
+    </div>
+  </div>
   <div
     class="control"
     class:enabled
@@ -52,17 +65,61 @@
 <style lang="scss">
 
   .joystick {
+    position: relative;
     width: 100%;
     height: 100%;
-    display: grid;
-    place-content: center;
+  }
+
+  .frame {
+    --size: 90px;
+    position: absolute;
+    width: var(--size);
+    height: var(--size);
+    top: calc(50% - var(--size) * 0.5);
+    left: calc(50% - var(--size) * 0.5);
+    touch-action: none;
+    background-color: rgba(0 0 0 / 0.2);
+    border-radius: 50%;
+    display: flex;
+    flex-direction: column;
+
+    .up,
+    .middle,
+    .bottom {
+      flex: 1 1 0;
+      display: flex;
+    }
+
+    .up,
+    .bottom {
+      justify-content: center;
+    }
+
+    .middle {
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .up {
+      align-items: flex-start;
+    }
+
+    .bottom {
+      align-items: flex-end;
+    }
   }
 
   .control {
-    width: 50px;
-    height: 50px;
+    --size: 50px;
+    position: absolute;
+    z-index: 2;
+    width: var(--size);
+    height: var(--size);
+    top: calc(50% - var(--size) * 0.5);
+    left: calc(50% - var(--size) * 0.5);
     background-color: var(--control-idle-color);
     border-radius: 50%;
+    touch-action: none;
 
     &.enabled {
       background-color: var(--control-enabled-color);
